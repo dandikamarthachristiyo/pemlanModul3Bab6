6468b43e5fd6ec722ab5082897c49a8ad6e692d7
dandikamarthachristiyo <dandika.martha@gmail.com>
2026-03-30 09:55:23 +0700

    Manusia C.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         // Manusia Haris = new Manusia("Haris", "0123",true, true);
         // System.out.println(Haris);
-        Manusia Siti = new Manusia("Siti", "1234", false, true);
-        System.out.println(Siti);
+        //Manusia Siti = new Manusia("Siti", "1234", false, true);
+        //System.out.println(Siti);
+        Manusia Dika = new Manusia("Dika", "9876", true, false);
+        System.out.println(Dika);
     }
 }
